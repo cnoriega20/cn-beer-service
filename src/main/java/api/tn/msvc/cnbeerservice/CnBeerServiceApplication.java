494483b5c558f13cb5c2e89601453ff67f1f0063
cnoriega20 <c79.noriega@gmail.com>
2020-08-29 13:13:08 -0500

    First commit:Project Structure, added jaxb dependencies to pom and gitignore edits.

---
+++ src/main/java/api/tn/msvc/cnbeerservice/CnBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package api.tn.msvc.cnbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/tn/msvc/cnbeerservice/CnBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package api.tn.msvc.cnbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
