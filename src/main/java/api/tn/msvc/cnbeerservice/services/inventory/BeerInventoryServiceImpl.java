a537cf04d0fde27285785d9dc3ca50252b8ad641
cnoriega20 <c79.noriega@gmail.com>
2020-10-24 03:38:07 -0500

    Added new BeerInventoryDto and complete service method in BeerInventoryServiceImpl

--- src/main/java/api/tn/msvc/cnbeerservice/services/inventory/BeerInventoryServiceImpl.java
+++ src/main/java/api/tn/msvc/cnbeerservice/services/inventory/BeerInventoryServiceImpl.java
@@ -1,18 +1,53 @@
 package api.tn.msvc.cnbeerservice.services.inventory;
 
+import api.tn.msvc.cnbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-
+@Slf4j
 @ConfigurationProperties(prefix = "cn.brewery", ignoreUnknownFields = false)
 @Component
 public class BeerInventoryServiceImpl implements BeerInventoryService {
 
-    public static final String INVENTORY_PATH = "api/v1/beer/{beerId}/inventory";
+    private static final String INVENTORY_PATH = "api/v1/beer/{beerId}/inventory";
+    private final RestTemplate restTemplate;
+
+    private String beerInventoryServiceHost;
+
+
+
+    public BeerInventoryServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void setBeerInventoryServiceHost(String beerInventoryServiceHost) {
+        this.beerInventoryServiceHost = beerInventoryServiceHost;
+    }
+
     @Override
     public Integer getOnHandInventory(UUID beerId) {
-        return null;
+        log.debug("Calling inventory service");
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity =
+                restTemplate.exchange(beerInventoryServiceHost + INVENTORY_PATH,
+                        HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<BeerInventoryDto>>() {},
+                        (Object) beerId);
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        return onHand;
     }
 }
---
+++ src/main/java/api/tn/msvc/cnbeerservice/services/inventory/model/BeerInventoryDto.java
@@ -0,0 +1,22 @@
+package api.tn.msvc.cnbeerservice.services.inventory.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerInventoryDto {
+    private UUID id;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private UUID beerId;
+    private Integer quantityOnHand;
+
+}
