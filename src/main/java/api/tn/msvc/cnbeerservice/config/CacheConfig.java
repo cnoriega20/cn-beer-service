53fcc8975ddaab23c87549df215cd1d0b1341560
cnoriega20 <c79.noriega@gmail.com>
2021-01-02 12:45:44 -0600

    Added Cache configuration.

---
+++ src/main/java/api/tn/msvc/cnbeerservice/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package api.tn.msvc.cnbeerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
